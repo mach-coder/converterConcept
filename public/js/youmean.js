16b181dbea26f29db493f585e9b9654eac663530
ahmad athamneh <ahmad.athamneh@mawdoo3.com>
2021-10-09 06:31:51 +0300

    initial works

--- public/js/youmean.js
+++ public/js/youmean.js
@@ -3,16 +3,12 @@ const   socket = io.connect('')
         replacement = document.querySelector('.replacement'),
         message = document.getElementById('message');
 message.addEventListener('keyup', () => socket.emit('converting', { message: message.value }) );
-// setTimeout(() => {
-    socket.on('converting', function(data){
-        mean.hidden = false;
-        replacement.innerHTML = data.message;
-        replacement.onclick = () =>{
-            mean.hidden = true;
-            message.value = data.message
-            replacement.innerHTML = '';
-
-        };
-    });
-    
-// });
+socket.on('converting', function(data){
+    mean.hidden = false;
+    replacement.innerHTML = data.message;
+    replacement.onclick = () =>{
+        mean.hidden = true;
+        message.value = data.message
+        replacement.innerHTML = '';
+    };
+});
